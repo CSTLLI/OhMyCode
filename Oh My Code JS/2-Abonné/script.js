ede91942862d2d0f5a41cc7f4b0072a24bf7193a
CASTELLI Valerio <vcastell@student.s19.be>
2021-12-13 17:08:07 +0100

    Push du chapitre 2

--- Oh My Code JS/2-Abonné/script.js
+++ Oh My Code JS/2-Abonné/script.js
@@ -1,8 +1,11 @@
+// ### Test de liaison avec la page HTML
 console.log('connecté !');
 
+// ### Définition des variables
 const icone = document.querySelector('i');
 const btn = document.querySelector('.btn');
 
+// ### Evénèments 
 icone.addEventListener('click', function(){
 	icone.classList.toggle('fa-smile-wink');
 	icone.classList.toggle('happy');
