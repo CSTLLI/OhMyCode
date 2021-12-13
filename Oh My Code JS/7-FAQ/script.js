759df0f3d1af318d0384d551c7c77f1869d3d409
CASTELLI Valerio <vcastell@student.s19.be>
2021-12-13 17:09:26 +0100

    Push du chapitre 7

--- Oh My Code JS/7-FAQ/script.js
+++ Oh My Code JS/7-FAQ/script.js
@@ -1,7 +1,10 @@
+// ### Test de liaison avec la page HTML
 console.log('connecté !');
 
+// ### Définition des variables
 const questions = document.querySelectorAll('.question');
 
+// ### Evénèments 
 questions.forEach((item)=>{
 	item.addEventListener('click', function(){
 		const next = item.nextElementSibling;
