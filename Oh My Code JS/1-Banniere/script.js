e01aa85c0f389d17f50777e78e52fd3e44b1bd45
CASTELLI Valerio <vcastell@student.s19.be>
2021-12-13 17:07:51 +0100

    Push du chapitre 1

--- Oh My Code JS/1-Banniere/script.js
+++ Oh My Code JS/1-Banniere/script.js
@@ -1,11 +1,16 @@
+// Test de liaison avec la page HTML
 console.log('connecté !');
 
+
+// ### Définition des variables
 const btnSuccess = document.querySelector('.btn-success');
 const cookies = document.querySelector('.cookies');
 
+// ### Test avec la console
 console.log(btnSuccess);
 console.log(cookies);
 
+// ### Evénèments 
 btnSuccess.addEventListener('click', function(){
-	cookies.style.opacity = "0";
+	cookies.style.bottom = "-145px";
 });
