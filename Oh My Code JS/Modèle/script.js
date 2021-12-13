14402c7d8e2906384513efaf6150a1d607e94842
CASTELLI Valerio <vcastell@student.s19.be>
2021-12-13 17:04:25 +0100

    Mise à jour du modèle avec des commentaires

--- Oh My Code JS/Modèle/script.js
+++ Oh My Code JS/Modèle/script.js
@@ -1,3 +1,10 @@
-// Test de liaison avec la page HTML
+// ### Test de liaison avec la page HTML
 console.log('connecté !');
 
+// ### Définition des variables
+
+// ### Test avec la console
+
+// ### Fonctions
+
+// ### Evénèments 
