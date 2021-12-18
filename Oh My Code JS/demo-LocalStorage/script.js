4d1700aa1e3fb5dc6ad2145afccf986adbeec5a7
CASTELLI Valerio <vcastell@student.s19.be>
2021-12-18 10:52:36 +0100

    Push de la demo LocalStorage

---
+++ Oh My Code JS/demo-LocalStorage/script.js
@@ -0,0 +1,12 @@
+// ### Test de liaison avec la page HTML
+console.log('connecté !');
+
+// ### Définition des variables
+ 
+// ### Evénèments 
+
+// ### Création d'un stockage sous la forme clé/valeur
+// localStorage.setItem('nom', 'Durand');
+
+const stockage = window.localStorage;
+console.log(localStorage.getItem('nom'));
